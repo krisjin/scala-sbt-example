b22042ebb40de3d9737bafcf2d9459570f23f8b9
krisjin <krisibm@163.com>
2014-07-29 16:01:10 +0800

    clean

--- build.sbt
+++
@@ -1,7 +0,0 @@
-name := "scala-sbt-example"
-
-version := "1.0"
-
-scalaVersion := "2.10.4"
-
-libraryDependencies += "org.mongodb" %% "casbah" % "2.7.2"
--- src/main/scala/HelloWorldScala.scala
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 14-7-29.
- */
-object HelloWorldScala {
-  def main(args:Array[String])={
-    println("hello world, Scala!")
-  }
-}
