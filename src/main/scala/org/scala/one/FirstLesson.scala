115a30157503624694b4912cea9fb1a61d66d638
krisjin <krisibm@163.com>
2014-07-29 17:17:28 +0800

    val var

--- src/main/scala/org/scala/one/FirstLesson.scala
+++ src/main/scala/org/scala/one/FirstLesson.scala
@@ -1,14 +1,18 @@
 package org.zeus.scala.lesson1;
 /**
- * first lesson
- * 变量
+ * var:变量
+ * val:常量
  */
 object FirstLesson {
+
+ var name ="krisjin";
+ val status=1;
+ val answer = 8 * 5 + 2;
 	def main(args: Array[String]) {
 		val answer = 8 * 5 + 2;
+    name="kris";
 		println(answer);
-
-
-
+    println(name);
+    status=2;
 	}
 }
