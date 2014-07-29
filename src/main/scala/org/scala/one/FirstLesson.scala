5645bbd653e47d7cdf6131c48a4c7202dcbc7375
krisjin <krisibm@163.com>
2014-07-29 17:01:57 +0800

    clean

---
+++ src/main/scala/org/scala/one/FirstLesson.scala
@@ -0,0 +1,14 @@
+package org.zeus.scala.lesson1;
+/**
+ * first lesson
+ * 变量
+ */
+object FirstLesson {
+	def main(args: Array[String]) {
+		val answer = 8 * 5 + 2;
+		println(answer);
+
+
+
+	}
+}
