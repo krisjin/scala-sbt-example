b7686049cd662b06a3e974f2025442e8123614c1
root <123>
2014-08-04 21:38:09 +0800

    String

--- src/main/scala/scala/one/StringTest.scala
+++ src/main/scala/scala/one/StringTest.scala
@@ -1,12 +1,35 @@
 package scala.one
-
+import scala.math
+import scala.util.Random
 /**
  * Created by root on 14-8-3.
  */
 object StringTest {
 
   def main(args:Array[String]){
-    print("Hello".intersect("World"))
+    var str="scala"
+    var num:BigInt=12399
+    var seciNum:BigDecimal=12345
+    var num$=6
+    val num3:BigInt=9333
+    println("Hello".intersect("World"))
+
+    println(str.toLowerCase())
+    println(str.toUpperCase())
+    println(str.concat(" hello!"))
+    println(str.drop(1))
+    println("distinct:"+str.distinct)
+    println("get char:"+str(1))
+    println("scala patch:"+str.patch(2,"hool",4))
+    str=num.toString
+    println(str)
+
+
+    println(num*num*num*num)
+    println(seciNum*seciNum*seciNum*seciNum*seciNum)
+    println(BigInt.probablePrime(100,Random))
+
+    println(math.sqrt(num$))
   }
 
 }
