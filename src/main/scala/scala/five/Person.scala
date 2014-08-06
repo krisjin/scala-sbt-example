a0bec2ba5a2a0f448283315fa86a7b1b8a8b8153
krisjin <krisibm@163.com>
2014-08-06 14:29:51 +0800

    class object

---
+++ src/main/scala/scala/five/Person.scala
@@ -0,0 +1,10 @@
+package scala.five
+
+/**
+ * Created by kris on 2014/8/6.
+ */
+class Person(name: String, age: Int) {
+    println("name:"+name+"; age:"+age)
+    def say= println("hello!")
+
+}
---
+++ src/main/scala/scala/five/PersonTest.scala
@@ -0,0 +1,10 @@
+package scala.five
+
+/**
+ * Created by kris on 2014/8/6.
+ */
+object PersonTest {
+  def main(args: Array[String]) {
+    val person = new Person("krisjin", 22)
+  }
+}
