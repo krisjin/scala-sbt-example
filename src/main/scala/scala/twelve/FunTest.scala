921281549d2e9fbcfbce36b2cc1d7b3ab9deef75
krisjin <krisibm@163.com>
2014-08-14 17:13:31 +0800

    twelve

--- src/main/scala/scala/twelve/FunTest.scala
+++ src/main/scala/scala/twelve/FunTest.scala
@@ -10,8 +10,10 @@ object FunTest extends App {
   val num = 3.14
   val fun = ceil _
   val arrs = Array(3.14, 7.68, 45, 3).map(fun)
-  val c = (x: Double) => x * 3
+  val fc = (x: Double) => x * 3
+  //def fc(x: Double) = 3 * x
 
+  val arrs2 = Array(1.23, 2.34, 7.56).map((x: Double) => x * 2)
 
-  println(c(2))
+  println(arrs2.mkString(" "))
 }
